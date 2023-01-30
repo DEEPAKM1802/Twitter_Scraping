3cd53544f8ae04a5c40e4a864175db04567907f1
Deepak Mishra <116501015+DEEPAKM1802@users.noreply.github.com>
2023-01-30 09:48:35 +0530

    good coding practices

--- twitter_scraping.py
+++ twitter_scraping.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+#**********************************************************************************************************************************************
 
 #importing important packages
 import snscrape.modules.twitter as sntwitter
@@ -12,7 +12,8 @@ import datetime
 import os
 
 
-# In[12]:
+#**********************************************************************************************************************************************
+
 
 3 #function used for twiter scraping
 def twiter_scraping(query, limit):
@@ -24,14 +25,15 @@ def twiter_scraping(query, limit):
             break
         else:
             tweets.append([query, datetime.datetime.now(), tweet.date, tweet.id, tweet.url, tweet.content,  tweet.username, tweet.replyCount, tweet.retweetCount, tweet.lang, tweet.source, tweet.likeCount])   
-
+    
     #Storing the scrapped data into dataframe(i.e. df)       
     df = pd.DataFrame(tweets, columns=['Search Query', 'Timestamp', 'date', 'id', 'url', 'tweet content', 'user', 'reply count', 'retweet count', 'language', 'source', 'like count'])
     
     return df
 
 
-# In[13]:
+#**********************************************************************************************************************************************
+
 
 #function to upload data into mongo db (Please enter mongo url in conn)
 def df_to_mongo(df, conn="****************************************************************************************"):
@@ -40,70 +42,65 @@ def df_to_mongo(df, conn="******************************************************
     collection = db['twiter']
     collection.insert_many(df.to_dict('records'))
 
+    
+#**********************************************************************************************************************************************
 
-# In[ ]:
 
 #streamlit function for UI
 def streamlit_UI():
 
-    #Input fields to be dispalyed
+    #Title and header to be dispalyed
     colT1,colT2 = st.columns([1,3])
     with colT2:
         st.title(' :blue[Twitter Scraping]')
     header2 = st.subheader("Welcome!!! Please enter data to be searched")
-    
-    
+ 
+
+    #Input field with submit button( form)
     with st.form("my_form"):
          col1, col2, col3 = st.columns(3)
-
          with col1:
              text = st.text_input('Text', placeholder="Enter Text", disabled=False, label_visibility="visible")
              stdate = st.date_input("From Date", disabled=False, label_visibility="visible")
-       
-
          with col2:
               hata = st.text_input('#Hastags', placeholder="Enter Hastags", disabled=False, label_visibility="visible")
               eddate = st.date_input("To Date", disabled=False, label_visibility="visible")
-       
-
          with col3:
                user = st.text_input('User', placeholder="Enter Username", disabled=False, label_visibility="visible")
                limit = st.number_input("No. of records", disabled=False, label_visibility="visible")
          submitted = st.form_submit_button("Submit")
-    
+ 
+
     #Concatenate the input to be searched
     query = f"{text} until:{eddate} since:{stdate}"
     if hata != "":
         query = f"{text} (#{hata}) until:{eddate} since:{stdate}"
     if user != "":
        query = f"{text} (#{hata}) (from:{user}) until:{eddate} since:{stdate}"   
-    
     limit = int(limit)
 
     #Calling twiter function to scrape data based on the user input
     data = twiter_scraping(query, limit)
-    
-    col1, col2, col3 = st.columns(3)
+  
 
+    #Displaying upload, and dowload buttons
+    col1, col2, col3 = st.columns(3)
     with col1:
         if st.button("Upload"):
          df_to_mongo(df=data)
          st.success("Data uploaded successfully")
-            
-       
-
     with col2:
        st.download_button("Dowload CSV", data=data.to_csv(header=True, index=True), file_name="export_csv", on_click=None, disabled=False)
-       
-
     with col3:
        st.download_button("Dowload json", data=data.to_json(), file_name="export_json", on_click=None, disabled=False)
 
-    #Display output and buttons
+    
+    #Display output dataframe
     st.dataframe(data = data) 
 
+    
+#**********************************************************************************************************************************************
 
-# In[ ]:
 
 #Main call to UI which leads to excution of entire program
 maincall = streamlit_UI()
