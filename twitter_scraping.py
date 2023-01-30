652b6f4ba051e291800b30c76437ee6d456d2d48
Deepak Mishra <116501015+DEEPAKM1802@users.noreply.github.com>
2023-01-30 07:44:15 +0530

    Update twitter_scraping.py

--- twitter_scraping.py
+++ twitter_scraping.py
@@ -36,7 +36,8 @@ def twiter_scraping(query, limit):
 
 
 #The below function uploads the dataframe into mongodb
-def df_to_mongo(df, conn="mongodb://deepakmongo:*****@ac-hmiqjuj-shard-00-00.vk1yvh9.mongodb.net:27017,ac-hmiqjuj-shard-00-01.vk1yvh9.mongodb.net:27017,ac-hmiqjuj-shard-00-02.vk1yvh9.mongodb.net:27017/?ssl=true&replicaSet=atlas-wzamdc-shard-0&authSource=admin&retryWrites=true&w=majority"):
+#Please enter your monogo connection url in conn.
+def df_to_mongo(df, conn="*****************"):
     client = pymongo.MongoClient(conn)
     db = client['tweets']
     collection = db['twiter']
