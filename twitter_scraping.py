1abe9fd780e5e2db162ae437c4a74921363a80aa
Deepak Mishra <116501015+DEEPAKM1802@users.noreply.github.com>
2023-01-30 09:18:16 +0530

    with form

--- twitter_scraping.py
+++ twitter_scraping.py
@@ -4,18 +4,17 @@
 # In[2]:
 
 
-#Importing all the necnecessary packages
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
 import pymongo
 import streamlit as st
 import datetime
+import os
 
 
 # In[12]:
 
 
-#The following function scrapes twiter based on query given
 def twiter_scraping(query, limit):
     
     tweets = []
@@ -35,9 +34,7 @@ def twiter_scraping(query, limit):
 # In[13]:
 
 
-#The below function uploads the dataframe into mongodb
-#Please enter your monogo connection url in conn.
-def df_to_mongo(df, conn="*****************"):
+def df_to_mongo(df, conn="mongodb://deepakmongo:!AMDEEPAK1802@ac-hmiqjuj-shard-00-00.vk1yvh9.mongodb.net:27017,ac-hmiqjuj-shard-00-01.vk1yvh9.mongodb.net:27017,ac-hmiqjuj-shard-00-02.vk1yvh9.mongodb.net:27017/?ssl=true&replicaSet=atlas-wzamdc-shard-0&authSource=admin&retryWrites=true&w=majority"):
     client = pymongo.MongoClient(conn)
     db = client['tweets']
     collection = db['twiter']
@@ -47,40 +44,66 @@ def df_to_mongo(df, conn="*****************"):
 # In[ ]:
 
 
-#Streamlit function for UI
 def streamlit_UI():
 
     #Input fields to be dispalyed
-    Title = st.title("Twiter Scrapping")
-    header1 = st.header("Welcome to twiter scrapping application")
-    header2 = st.subheader("Please enter data to be searched")
-    text = st.text_input('Text', placeholder="Enter Text", disabled=False, label_visibility="visible")
-    hata = st.text_input('#Hastags', placeholder="Enter Hastags", disabled=False, label_visibility="visible")
-    user = st.text_input('User', placeholder="Enter Username", disabled=False, label_visibility="visible")
-    stdate = st.date_input("From Date", disabled=False, label_visibility="visible")
-    eddate = st.date_input("To Date", disabled=False, label_visibility="visible")
-    limit = st.number_input("No. of records", disabled=False, label_visibility="visible")
+    colT1,colT2 = st.columns([1,3])
+    with colT2:
+        st.title(' :blue[Twitter Scraping]')
+    header2 = st.subheader("Welcome!!! Please enter data to be searched")
+    
+    
+    with st.form("my_form"):
+         col1, col2, col3 = st.columns(3)
 
+         with col1:
+             text = st.text_input('Text', placeholder="Enter Text", disabled=False, label_visibility="visible")
+             stdate = st.date_input("From Date", disabled=False, label_visibility="visible")
+       
+
+         with col2:
+              hata = st.text_input('#Hastags', placeholder="Enter Hastags", disabled=False, label_visibility="visible")
+              eddate = st.date_input("To Date", disabled=False, label_visibility="visible")
+       
+
+         with col3:
+               user = st.text_input('User', placeholder="Enter Username", disabled=False, label_visibility="visible")
+               limit = st.number_input("No. of records", disabled=False, label_visibility="visible")
+         submitted = st.form_submit_button("Submit")
+    
     #Concatenate the input to be searched
-    query = f"{text} (#{hata}) (from:{user}) until:{stdate} since:{eddate}"
+    query = f"{text} until:{eddate} since:{stdate}"
+    if hata != "":
+        query = f"{text} (#{hata}) until:{eddate} since:{stdate}"
+    if user != "":
+       query = f"{text} (#{hata}) (from:{user}) until:{eddate} since:{stdate}"   
+    
     limit = int(limit)
 
     #Calling twiter function to scrape data based on the user input
     data = twiter_scraping(query, limit)
     
-    #Display output and buttons
-    st.dataframe(data = data)  
+    col1, col2, col3 = st.columns(3)
 
-    if st.button("Upload"):
+    with col1:
+        if st.button("Upload"):
          df_to_mongo(df=data)
+         st.success("Data uploaded successfully")
             
-    st.download_button("Dowload CSV", data=data.to_csv(header=True, index=True), file_name="export_csv", on_click=None, disabled=False)
-    
-    st.download_button("Dowload json", data=data.to_json(), file_name="export_json", on_click=None, disabled=False)
+       
+
+    with col2:
+       st.download_button("Dowload CSV", data=data.to_csv(header=True, index=True), file_name="export_csv", on_click=None, disabled=False)
+       
+
+    with col3:
+       st.download_button("Dowload json", data=data.to_json(), file_name="export_json", on_click=None, disabled=False)
+
+    #Display output and buttons
+    st.dataframe(data = data) 
 
 
 # In[ ]:
 
 
 maincall = streamlit_UI()
-
